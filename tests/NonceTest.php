4e627043b64e9d64dea7f4d797f3e0e8041b5287
Rouven Hurling <rouven@rhurling.de>
2016-03-26 14:04:32 +0100

    Remove most of ConfigurableTrait funcitons ouf of NonceTest

--- tests/NonceTest.php
+++ tests/NonceTest.php
@@ -16,7 +16,15 @@ class NonceTest extends \PHPUnit_Framework_TestCase
 {
 
     static $time = 1458891857;
-    static $nonce = '826e26509a';
+    static $nonce = 'c9b9978685';
+
+    public static function setUpBeforeClass()
+    {
+        Config::setLifespan(86400);
+        Config::setSalt('salt');
+        Config::setUserId(1);
+        Config::setSessionToken('session-1');
+    }
 
     public function setUp()
     {
@@ -26,12 +34,8 @@ class NonceTest extends \PHPUnit_Framework_TestCase
     public function testCreation()
     {
         $nonce = new Nonce();
-
-        $this->assertEquals(self::$nonce, (string)$nonce, 'String casting generation');
-
-        $nonce = $nonce->generate();
-
-        $this->assertEquals(self::$nonce, $nonce, 'Basic Nonce generation');
+        $this->assertEquals(self::$nonce, $nonce->generate());
+        $this->assertEquals(self::$nonce, (string)$nonce);
     }
 
     public function testVerification()
@@ -51,91 +55,12 @@ class NonceTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($nonce->verify(self::$nonce), 'Nonce older than 24 hours');
     }
 
-    static $userId = 1;
-    static $userNonce = '32300ab40b';
-
-    public function testUserSpecificCreation()
+    public static function tearDownAfterClass()
     {
-        $nonce = new Nonce();
-        $nonce = $nonce->setUserId(self::$userId)->generate();
-
-        $this->assertNotEquals(self::$nonce, $nonce, 'User Nonce is different than basic Nonce');
-        $this->assertEquals(self::$userNonce, $nonce, 'User Nonce is as expected');
-
-        $nonce2 = new Nonce();
-        $nonce2 = $nonce2->setUserId(self::$userId + 1)->generate();
-
-        $this->assertNotEquals($nonce, $nonce2, 'User Nonce is different than User 2 Nonce');
-    }
-
-    static $sessionToken = 'session56f5235017cfc1.67463146';
-    static $sessionNonce = '9bd1d4a13c';
-
-    public function testSessionSpecificNonce()
-    {
-        $nonce = new Nonce();
-        $nonce = $nonce->setSessionToken(self::$sessionToken)->generate();
-
-        $this->assertNotEquals(self::$nonce, $nonce, 'Session Nonce is different than basic Nonce');
-        $this->assertEquals(self::$sessionNonce, $nonce, 'Session Nonce is as expected');
-
-        $nonce2 = new Nonce();
-        $nonce2 = $nonce2->setSessionToken(uniqid('session', true))->generate();
-
-        $this->assertNotEquals($nonce, $nonce2, 'Session Nonce is different than Session 2 Nonce');
-    }
-
-    static $salt = '6Op?ql<.n$mfX-t>}vHhNy([Z}P.gEhUj_r))U#ETbMxshr{YyZ7:~J,SW()``3E[NAzWO~7_k21xJ|Plvn/,Zz%=,jgf/9R|3}DI-1hIwMDi4OyjTT$S)J^;WOVe6&s';
-    static $saltNonce = '43461ff9b1';
-
-    public function testSaltCreation()
-    {
-        $nonce = new Nonce();
-        $nonce = $nonce->setSalt(self::$salt)->generate();
-
-        $this->assertNotEquals(self::$nonce, $nonce, 'Salt Nonce is different than basic Nonce');
-        $this->assertEquals(self::$saltNonce, $nonce, 'Salt Nonce is as expected');
-
-        $nonce2 = new Nonce();
-        $nonce2 = $nonce2->setSalt(openssl_random_pseudo_bytes(64))->generate();
-
-        $this->assertNotEquals($nonce, $nonce2, 'Salt Nonce is different than Salt 2 Nonce');
-    }
-
-    static $action = 'phpunit-testing';
-    static $actionNonce = '7668c4925d';
-
-    public function testActionCreation()
-    {
-        $nonce = new Nonce(self::$action);
-        $nonce = $nonce->generate();
-
-        $this->assertNotEquals(self::$nonce, $nonce, 'Action Nonce is different than basic Nonce');
-        $this->assertEquals(self::$actionNonce, $nonce, 'Action Nonce is as expected');
-
-        $nonce2 = new Nonce('phpunit-nonce2');
-        $nonce2 = $nonce2->generate();
-
-        $this->assertNotEquals($nonce, $nonce2, 'Action Nonce is different than Action 2 Nonce');
-    }
-
-    static $everythingNonce = '682c250ccb';
-
-    public function testCombinedCreation()
-    {
-        $nonce = new Nonce(self::$action);
-        $nonce = $nonce
-            ->setSalt(self::$salt)
-            ->setSessionToken(self::$sessionToken)
-            ->setUserId(self::$userId)
-            ->generate();
-
-        $this->assertNotEquals(self::$nonce, $nonce);
-        $this->assertNotEquals(self::$actionNonce, $nonce);
-        $this->assertNotEquals(self::$saltNonce, $nonce);
-        $this->assertNotEquals(self::$sessionNonce, $nonce);
-        $this->assertNotEquals(self::$userNonce, $nonce);
-        $this->assertEquals(self::$everythingNonce, $nonce);
+        Config::setLifespan(86400);
+        Config::setSalt(null);
+        Config::setUserId(null);
+        Config::setSessionToken(null);
     }
 
 }
